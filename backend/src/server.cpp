3312703f1f647974145123fd58011c2a420405e6
verkag <verkag1v@gmail.com>
2025-02-08 03:38:49 +0300

    basic logger functionality is implemented

--- backend/src/server.cpp
+++ backend/src/server.cpp
@@ -1,38 +1,70 @@
 #include "server.hpp"
 #include <iostream>
+
 #include "spdlog/async.h"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/stdout_sinks.h"
+
+Server::~Server() {
+    logger_->info("server is shutting down..");
+}
+
+Server::Server() : ioc_(), acceptor_(ioc_, {asio::ip::tcp::v4(), 5555}), signals_(ioc_) {
+    ltp_ = std::make_shared<spdlog::details::thread_pool>(8192, 1);
+    spdlog::set_level(spdlog::level::debug);
+
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_st>();
+    console_sink->set_pattern("[%^%l%$] %v");
+    console_sink->set_level(spdlog::level::info);
+
+    auto console_err_sink = std::make_shared<spdlog::sinks::stderr_color_sink_st>();
+    console_err_sink->set_pattern("[%^%l%$] %v");
+    console_err_sink->set_level(spdlog::level::err);
+
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_st>("logs/async_logs.txt", true);
+    file_sink->set_pattern("[%Y-%m-%d %H:%M:%S] [%l] %v");
+    file_sink->set_level(spdlog::level::debug);
+
+    spdlog::sinks_init_list sil = { console_sink, console_err_sink, file_sink };
+    logger_ = std::make_shared<spdlog::async_logger>("async_multisink_logger", sil.begin(), sil.end(), ltp_);
+    logger_->set_level(spdlog::level::debug);
 
 
-Server::Server() {
-    logger_ = spdlog::basic_logger_st<spdlog::async_factory>("async_logger", "logs/async_log.txt");
+    signals_.add(SIGINT);
+    signals_.add(SIGTERM);
+    signals_.async_wait([this](const boost::system::error_code& /*ec*/, int /*signal_number*/){
+        logger_->debug("signal_handler has been called");
+        acceptor_.close();
+    });
 }
 
 void Server::run() {
-    asio::io_context ioc;
 
-    asio::co_spawn(ioc, accept(), asio::detached);
+    logger_->info("server is starting up..");
+    asio::co_spawn(ioc_, accept(), asio::detached);
+    logger_->debug("co_spawn was passed");
 
-    ioc.run(); 
+    ioc_.run(); 
 }
 
 asio::awaitable<void> Server::accept() {
-    auto executor = co_await boost::asio::this_coro::executor;
-    asio::ip::tcp::acceptor acceptor(executor, {asio::ip::tcp::v4(), 5555});
-    acceptor.set_option(asio::socket_base::reuse_address());
+    auto executor = co_await boost::asio::this_coro::executor; // typically this executor would be transformed to asio::strand when switching to multithreaded model
+    logger_->info("server is now accepting connections.."); 
 
     for (;;) {
-        asio::ip::tcp::socket socket = co_await acceptor.async_accept(asio::use_awaitable);
+        asio::ip::tcp::socket socket = co_await acceptor_.async_accept(asio::use_awaitable);
         beast::tcp_stream stream(std::move(socket));
         asio::co_spawn(executor, process_session(std::move(stream)), asio::detached); 
     }   
 }
 
 asio::awaitable<void> Server::process_session(beast::tcp_stream stream) {
+    logger_->info("accepted the message");
     beast::flat_buffer buffer; 
 
     for(;;) {
-        stream.expires_after(std::chrono::seconds(30));
+        stream.expires_after(std::chrono::seconds(10));
         beast::http::request<beast::http::string_body> req;
 
         co_await beast::http::async_read(stream, buffer, req, asio::use_awaitable);
@@ -44,25 +76,22 @@ asio::awaitable<void> Server::process_session(beast::tcp_stream stream) {
         if (!keep_alive) break;
     }
 
+    // this section won't be called if keep_alive is true
+    logger_->info("server handled the connection");
     stream.socket().shutdown(asio::ip::tcp::socket::shutdown_send);
 }
 
 beast::http::message_generator Server::handle_request(beast::http::request<beast::http::string_body>&& req) {
-//   beast::http::response<beast::http::string_body> res{beast::http::status::ok, req.version()};
-//   res.body() = "123";
-//   res.content_length(3);
-//   res.keep_alive(req.keep_alive());
-//   return res;
     beast::http::response<beast::http::string_body> returned_res;
     for (auto& [k,v] : resources) {
-        std::cout << req.target() << std::endl;
-
         if (std::regex_search(static_cast<std::string>(req.target()), k)) {
-            v[req.method_string()](std::move(req), returned_res);
-            std::cout << "handler has been found" << std::endl;
+            const std::string method = req.method_string();
+            const std::string target = req.target();
+            v[method](std::move(req), returned_res);
+            logger_->debug("handler {} {} has been called", target, method); 
         }
     }
-
+    
     return returned_res;
 }
 
--- backend/src/server.hpp
+++ backend/src/server.hpp
@@ -5,6 +5,7 @@
 #include "util.hpp"
 
 #include <spdlog/spdlog.h>
+#include "spdlog/async.h"
 
 namespace asio = boost::asio;
 namespace beast = boost::beast;
@@ -15,12 +16,18 @@ class Server {
 public: 
     void run();
     Server();
-    // add logger logic here
+    ~Server();
+
     std::map<CustomRegex, std::map<std::string, std::function<void(const beast::http::request<string_body>, beast::http::response<string_body>&)>>> resources;
 private:
     asio::awaitable<void> accept(); 
     asio::awaitable<void> process_session(beast::tcp_stream stream); 
     beast::http::message_generator handle_request(beast::http::request<beast::http::string_body>&& req);
 
-    std::shared_ptr<spdlog::logger> logger_; 
+    std::shared_ptr<spdlog::async_logger> logger_; 
+    std::shared_ptr<spdlog::details::thread_pool> ltp_;
+
+    asio::io_context ioc_;
+    asio::ip::tcp::acceptor acceptor_;
+    asio::signal_set signals_;
 };
